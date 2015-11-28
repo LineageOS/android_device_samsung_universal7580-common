ac89a8f06ea3d573f165a7f60a6ffab96f4b11fe
codeworkx <codeworkx@cyanogenmod.org>
2015-11-28 20:33:54 +0100

    universal7580: camera: add empty camera wrapper
    Changes:
    * Removed references to "Camera3"
    * Made it build correctly on universal7580
    
    Change-Id: Ifa7a77ff5a9eb903cf2146d8ce8c7d6a64a6da4c
    Signed-off-by: Jesse Chan <jc@lineageos.org>

---
+++ camera/Android.mk
@@ -0,0 +1,20 @@
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := \
+    CameraWrapper.cpp \
+    Camera2Wrapper.cpp
+
+LOCAL_SHARED_LIBRARIES := \
+    libhardware liblog libcamera_client libutils libcutils
+
+LOCAL_C_INCLUDES += \
+    system/core/include \
+    system/media/camera/include
+
+LOCAL_MODULE_RELATIVE_PATH := hw
+
+LOCAL_MODULE := camera.$(TARGET_BOOTLOADER_BOARD_NAME)
+LOCAL_MODULE_TAGS := optional
+
+include $(BUILD_SHARED_LIBRARY)
