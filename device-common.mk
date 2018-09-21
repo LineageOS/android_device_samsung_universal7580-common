e2934e8aa20e6d81efa3797a84e2f07609c4b5eb
Stricted <info@stricted.net>
2018-09-22 00:25:27 +0200

    universal7580: build textclassifier bundle
    Change-Id: I326101539f689589e85691fe83e4076939f080e3

--- device-common.mk
+++ device-common.mk
@@ -82,6 +82,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.renderscript@1.0-impl
 
+# TextClassifier
+PRODUCT_PACKAGES += \
+    textclassifier.smartselection.bundle1
+
 # Wifi
 PRODUCT_PACKAGES += \
     macloader \
