5eddb9b6625ea70ee0579ddaeaa7cef4e0527fbd
Stricted <info@stricted.net>
2018-09-22 00:19:57 +0200

    universal7580: build DRM hal
    Change-Id: I78a0ba3932c6ad82d77fdd83c8f84a3b35531464

--- device-common.mk
+++ device-common.mk
@@ -40,6 +40,11 @@ PRODUCT_PACKAGES += \
     android.hardware.configstore@1.0-impl \
     android.hardware.configstore@1.0-service
 
+# DRM
+PRODUCT_PACKAGES += \
+    android.hardware.drm@1.0-impl \
+    android.hardware.drm@1.0-service
+
 # Graphics
 PRODUCT_PACKAGES += \
     libion \
