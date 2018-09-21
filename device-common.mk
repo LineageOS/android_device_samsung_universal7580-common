2211511a13529ed593856af2b75e61fbfca02d8c
Stricted <info@stricted.net>
2018-09-21 23:38:23 +0200

    universal7580: inherit proprietary vendor files
    Change-Id: Ie8a29dc571838c8fca20d062bf4c5cd62af41476

--- device-common.mk
+++ device-common.mk
@@ -19,3 +19,6 @@ LOCAL_PATH := device/samsung/universal7580-common
 # call Samsung LSI board support package
 $(call inherit-product, hardware/samsung_slsi-cm/exynos5/exynos5.mk)
 $(call inherit-product, hardware/samsung_slsi-cm/exynos7580/exynos7580.mk)
+
+# call the proprietary setup
+$(call inherit-product, vendor/samsung/universal7580-common/universal7580-common-vendor.mk)
