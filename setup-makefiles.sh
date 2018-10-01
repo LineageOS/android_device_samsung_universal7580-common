fd19796381b5d19581f525f6aad0bc24214a7a60
Stricted <info@stricted.net>
2018-10-01 07:14:02 +0200

    universal7580: Create vulkan symlinks
    Change-Id: Id79b8fbc0f9ec65de4900366022aa5cd72475694

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -13,7 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-set -e
 
 VENDOR=samsung
 DEVICE_COMMON=universal7580-common
@@ -41,6 +40,48 @@ write_headers "a3xelte a5xelte a7xelte gvwifi gvlte j7elte s5neolte"
 
 # The standard blobs
 write_makefiles "$MY_DIR"/proprietary-files.txt true
+
+###################################################################################################
+# CUSTOM PART START                                                                               #
+###################################################################################################
+OUTDIR=vendor/$VENDOR/$DEVICE_COMMON
+(cat << EOF) >> $LINEAGE_ROOT/$OUTDIR/Android.mk
+include \$(CLEAR_VARS)
+LOCAL_MODULE := libGLES_mali
+LOCAL_MODULE_OWNER := samsung
+LOCAL_SRC_FILES := proprietary/vendor/lib/egl/libGLES_mali.so
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_SUFFIX := .so
+LOCAL_MODULE_PATH := \$(TARGET_OUT_VENDOR_SHARED_LIBRARIES)/egl
+
+SYMLINKS := \$(TARGET_OUT)/vendor
+\$(SYMLINKS):
+	@echo "Symlink: vulkan.exynos5.so"
+	@mkdir -p \$@/lib/hw
+	\$(hide) ln -sf ../egl/libGLES_mali.so \$@/lib/hw/vulkan.exynos5.so
+	@echo "Symlink: libOpenCL.so"
+	\$(hide) ln -sf egl/libGLES_mali.so \$@/lib/libOpenCL.so
+	@echo "Symlink: libOpenCL.so.1"
+	\$(hide) ln -sf egl/libGLES_mali.so \$@/lib/libOpenCL.so.1
+	@echo "Symlink: libOpenCL.so.1.1"
+	\$(hide) ln -sf egl/libGLES_mali.so \$@/lib/libOpenCL.so.1.1
+
+ALL_MODULES.\$(LOCAL_MODULE).INSTALLED := \\
+	\$(ALL_MODULES.\$(LOCAL_MODULE).INSTALLED) \$(SYMLINKS)
+
+include \$(BUILD_PREBUILT)
+
+EOF
+
+(cat << EOF) >> $LINEAGE_ROOT/$OUTDIR/$DEVICE_COMMON-vendor.mk
+
+# Create Mali links for Vulkan and OpenCL
+PRODUCT_PACKAGES += libGLES_mali
+EOF
+###################################################################################################
+# CUSTOM PART END                                                                                 #
+###################################################################################################
 
 # Done
 write_footers
