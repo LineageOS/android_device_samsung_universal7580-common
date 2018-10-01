7e077bee84073a99428914d816ed0805c2287331
Stricted <info@stricted.net>
2018-10-01 11:54:00 +0200

    universal7580: build sensor packages
    Change-Id: Iff1ceee2642219851e273f61529914a14a010d06

--- device-common.mk
+++ device-common.mk
@@ -135,6 +135,12 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/seccomp/mediaextractor-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaextractor.policy \
     $(LOCAL_PATH)/seccomp/mediacodec-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy
 
+# Sensors
+PRODUCT_PACKAGES += \
+    android.hardware.sensors@1.0-impl \
+    android.hardware.vibrator@1.0-impl \
+    android.hardware.vibrator@1.0-service
+
 # Shims
 PRODUCT_PACKAGES += \
     libcamera_client_shim \
