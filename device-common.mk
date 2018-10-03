e148bed632db65cfb819d89b747d6293116e4183
Stricted <info@stricted.net>
2018-10-03 04:45:52 +0200

    universal7580: update bsp config flags
    Change-Id: I48dae62d85a545aaf8626b38045db0dc71b4a3e5

--- device-common.mk
+++ device-common.mk
@@ -182,8 +182,8 @@ PRODUCT_COPY_FILES += \
 -include $(LOCAL_PATH)/system_prop.mk
 
 # call Samsung LSI board support package
-$(call inherit-product, hardware/samsung_slsi-cm/exynos5/exynos5.mk)
-$(call inherit-product, hardware/samsung_slsi-cm/exynos7580/exynos7580.mk)
+$(call inherit-product, hardware/samsung_slsi/exynos5/exynos5.mk)
+$(call inherit-product, hardware/samsung_slsi/exynos7580/exynos7580.mk)
 
 # call the proprietary setup
 $(call inherit-product, vendor/samsung/universal7580-common/universal7580-common-vendor.mk)
