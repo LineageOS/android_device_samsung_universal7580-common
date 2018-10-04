c1a504889130f8b5ae64e9c2ee39ca845363703e
Danny Wood <danwood76@gmail.com>
2018-10-04 09:12:58 +0200

    universal7580: sepolicy: Add initial universal7580 SELinux policy
    Change-Id: I8dd2eefba25e500edcb08b453fff6da1a33fbe30

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -153,6 +153,10 @@ BACKLIGHT_PATH := "/sys/class/backlight/panel/brightness"
 # Recovery
 TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/ramdisk/etc/fstab.samsungexynos7580
 
+# SELinux
+BOARD_SEPOLICY_DIRS += device/samsung/universal7580-common/sepolicy 
+BOARD_SEPOLICY_VERS := $(PLATFORM_SDK_VERSION).0
+
 # Shims
 TARGET_LD_SHIM_LIBS := \
     /system/lib/omx/libOMX.Exynos.AVC.Decoder.so|/vendor/lib/libui_shim.so \
