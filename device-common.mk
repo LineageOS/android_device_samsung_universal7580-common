8d217709c58171d271624fdb749fd526ce606688
Lukas0610 <mail@lukasberger.at>
2018-11-06 15:17:59 +0100

    universal7580: add device-specific bluetooth HIDL implementation
    Change-Id: I0a239cfd728fe3fcee4bbbb7f557becad666935a

--- device-common.mk
+++ device-common.mk
@@ -34,7 +34,7 @@ PRODUCT_COPY_FILES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
-    android.hardware.bluetooth@1.0-impl \
+    android.hardware.bluetooth@1.0-impl.7580 \
     libbt-vendor
 
 # Camera
