f7e2172ba051fea1dea5fbd6a1cfa56dc2422019
Joey <joey@lineageos.org>
2019-01-13 10:21:11 +0100

    universal7580: Build Trust HAL
    Change-Id: I92706987f8d3094cdfb82e71e5331b7efe69e5df

--- device-common.mk
+++ device-common.mk
@@ -164,6 +164,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.touch@1.0-service.samsung
 
+# Trust HAL
+PRODUCT_PACKAGES += \
+    vendor.lineage.trust@1.0-service
+
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service.basic
