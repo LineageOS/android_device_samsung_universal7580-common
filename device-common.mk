ab3bdefd0ca5d6ea9d32d44ac10c2b3e017399ed
Dario Trombello <dariotr@outlook.de>
2020-03-11 15:16:24 +0100

    universal7580: device-common: Uprevision Audio HAL to 5.0
    Change-Id: Ibd0400162a76a4eb8121559fb2d2ee232dc4fa16

--- device-common.mk
+++ device-common.mk
@@ -25,8 +25,8 @@ PRODUCT_PACKAGES += \
     audio.r_submix.default \
     audio.usb.default \
     tinymix \
-    android.hardware.audio@2.0-impl \
-    android.hardware.audio.effect@2.0-impl
+    android.hardware.audio@5.0-impl \
+    android.hardware.audio.effect@5.0-impl
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/audio_policy.conf:system/etc/audio_policy.conf \
