19ab13dc6e0e1bc9aa007ba8401609472a99bd93
Jan Altensen <info@stricted.net>
2020-04-10 22:37:14 +0200

    universal7580: remove libui shim
    Change-Id: I0eb1af28adcfa8becf95230170e5b21ac4fdb9f1

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -189,13 +189,3 @@ BOARD_SEPOLICY_VERS := $(PLATFORM_SDK_VERSION).0
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += $(LOCAL_PATH)
-
-# Shims
-TARGET_LD_SHIM_LIBS := \
-    /system/lib/omx/libOMX.Exynos.AVC.Decoder.so|/vendor/lib/libui_shim.so \
-    /system/lib/omx/libOMX.Exynos.AVC.Encoder.so|/vendor/lib/libui_shim.so \
-    /system/lib/omx/libOMX.Exynos.MPEG4.Decoder.so|/vendor/lib/libui_shim.so \
-    /system/lib/omx/libOMX.Exynos.MPEG4.Encoder.so|/vendor/lib/libui_shim.so \
-    /system/lib/omx/libOMX.Exynos.VP8.Decoder.so|/vendor/lib/libui_shim.so \
-    /system/lib/omx/libOMX.Exynos.VP8.Encoder.so|/vendor/lib/libui_shim.so \
-    /system/lib/omx/libOMX.Exynos.WMV.Decoder.so|/vendor/lib/libui_shim.so
