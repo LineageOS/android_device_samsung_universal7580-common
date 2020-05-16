9f7fb45f1110bb7f21e1ac1a6101b58c12929ed9
Danny Wood <danwood76@gmail.com>
2020-05-16 01:34:51 +0100

    universal7580: fix mediaserver libstagefright_shim path
    * shim moved by commit 9e8cffaafffa0a691476319ea03aa81edb4ccc85
    
    Change-Id: I9c5ba2e4786c72d295281758a9294b40c0e9bfaa

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -192,4 +192,4 @@ PRODUCT_SOONG_NAMESPACES += $(LOCAL_PATH)
 
 # Shim
 TARGET_LD_SHIM_LIBS += \
-    /system/bin/mediaserver|/vendor/lib/libstagefright_shim.so
+    /system/bin/mediaserver|/system/lib/libstagefright_shim.so
