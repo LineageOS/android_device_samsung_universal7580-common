303626917a7132970b7e9f0fa2c2709eb5a4a4bf
Danny Wood <danwood76@gmail.com>
2020-07-30 08:47:29 +0100

    universal7580: Add common GPS configuration
    * Configuration files from A510FXXS8CSF3
    
    Change-Id: I7df6e579731a5683a4181bdfbc7bb7c194e40db9

--- device-common.mk
+++ device-common.mk
@@ -66,6 +66,10 @@ PRODUCT_HOST_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.gnss@1.0-impl
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/gps/gps.conf:system/etc/gps.conf \
+    $(LOCAL_PATH)/configs/gps/gps.xml:system/etc/gps.xml
+
 # Graphics
 PRODUCT_PACKAGES += \
     libion \
