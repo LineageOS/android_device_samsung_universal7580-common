0013a69f2b61cba6dedd18b0b23adcdb27369c1c
Dario Trombello <dariotr@outlook.de>
2021-04-01 10:03:25 +0200

    universal7580: Decommonise LiveDisplay
    The J7 2015 doesn't support it
    
    Change-Id: I143da7f159d39933a745acc6cde9811c02c7ee83

--- device-common.mk
+++ device-common.mk
@@ -98,10 +98,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.samsung
 
-# Livedisplay
-PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service.samsung-exynos
-
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
