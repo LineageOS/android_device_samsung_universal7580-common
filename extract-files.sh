91adbad80764e1c5fccf4b155c0a996e55d49d26
Danny Wood <danwood76@gmail.com>
2021-07-22 13:36:49 +0100

    universal7580: Remove liboemcrypto to disable Widevine L1 support
    * L1 does not work on modified systems
    * Also switch to using the patched A510F widevine blob for working L3
    Change-Id: I6502528afd16aa42e566934022a94cf05a5edd3b
    
    Change-Id: I9a412a387a7d714cb65a631df14a93bc93921b21

--- extract-files.sh
+++ extract-files.sh
@@ -62,6 +62,7 @@ extract "${MY_DIR}/proprietary-files.txt" "${SRC}" "${KANG}" --section "${SECTIO
 # Fix proprietary blobs
 BLOB_ROOT="$ANDROID_ROOT"/vendor/"$VENDOR"/"$DEVICE_COMMON"/proprietary
 "${PATCHELF}" --replace-needed libgui.so libsensor.so $BLOB_ROOT/vendor/bin/gpsd
+"${PATCHELF}" --replace-needed libprotobuf-cpp-lite.so libprotobuf-cpp-fl26.so $BLOB_ROOT/vendor/lib/mediadrm/libwvdrmengine.so
 
 # replace SSLv3_client_method with SSLv23_method
 sed -i "s/SSLv3_client_method/SSLv23_method\x00\x00\x00\x00\x00\x00/" $BLOB_ROOT/vendor/bin/gpsd
